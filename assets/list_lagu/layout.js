5073a6604aa520d0196b6f29d8fad2b192861852
Bimachord <157880771+Bimachord@users.noreply.github.com>
2024-01-29 10:08:52 +0800

    Create layout.js

---
+++ assets/list_lagu/layout.js
@@ -0,0 +1,12 @@
+function showImage(imageSrc) {
+    var modal = document.getElementById('imageModal');
+    var modalImage = document.getElementById('modalImage');
+  
+    modal.style.display = 'block';
+    modalImage.src = imageSrc;
+  }
+  
+  function closeModal() {
+    var modal = document.getElementById('imageModal');
+    modal.style.display = 'none';
+  }
